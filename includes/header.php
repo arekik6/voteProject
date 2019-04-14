497cec965c6426073f0460833fdc7224a993a400
Ayoub Chabchoub <ayoub.chabchoub@insat.u-carthage.tn>
2019-04-14 23:48:21 +0100

    fix bootstrap bug temporairly

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@ $path = substr(__FILE__, strlen($_SERVER['DOCUMENT_ROOT']));
 $format_path = str_replace("\\","/",$path);
 $root = explode("/",$format_path)[1];
 ?>
-<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href=/node_modules/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js">
 </script>
